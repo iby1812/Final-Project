fca0a6ab7e8e555513bd464879aafaeb6d62f929
itay <itaybar7@gmail.com>
2019-03-24 17:00:39 +0200

    change drawer layout direction

--- app/src/main/java/com/cinema/cintix/HomePage.java
+++ app/src/main/java/com/cinema/cintix/HomePage.java
@@ -1,21 +1,19 @@
 package com.cinema.cintix;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
-
 import com.cinema.cintix.bottomnavigation.QuickOrder;
 import com.cinema.cintix.bottomnavigation.RegularOrder;
 import com.cinema.cintix.bottomnavigation.SmartOrder;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -35,6 +33,7 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
         setContentView(R.layout.activity_main);
         SetToolbar();
         SetBottomNavigator();
+        SetFragment(regularOrder);
     }
 
 
@@ -56,16 +55,16 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
     }
 
     private void closeDrawer() {
-        drawerLayout.closeDrawer(GravityCompat.START);
+        drawerLayout.closeDrawer(Gravity.RIGHT);
     }
 
     private void openDrawer() {
-        drawerLayout.openDrawer(GravityCompat.START);
+        drawerLayout.openDrawer(Gravity.RIGHT);
     }
 
     @Override
     public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+        if (drawerLayout.isDrawerOpen(Gravity.RIGHT)) {
             closeDrawer();
         } else super.onBackPressed();
     }
