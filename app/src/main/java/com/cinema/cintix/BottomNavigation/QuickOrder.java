b215722678230766ab3da82597f97f4a6b6cafa1
iby1812 <itaybar7@gmail.com>
2019-03-24 19:45:37 +0200

    bug

--- app/src/main/java/com/cinema/cintix/BottomNavigation/QuickOrder.java
+++ app/src/main/java/com/cinema/cintix/BottomNavigation/QuickOrder.java
@@ -1,4 +1,19 @@
-package com.cinema.cintix.BottomNavigation;
+package com.cinema.cintix.bottomnavigation;
 
-public class QuickOrder {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cinema.cintix.R;
+
+import androidx.fragment.app.Fragment;
+
+public class QuickOrder extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+          return inflater.inflate(R.layout.quick_order, container, false);
+    }
 }
+
